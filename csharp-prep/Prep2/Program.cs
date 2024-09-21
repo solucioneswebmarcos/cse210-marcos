32250eb2cc66052bdc75f31a457f31b76f16706c
solucioneswebmarcos <marcossud185@gmail.com>
2024-09-21 20:00:50 +0200

    complete de prep 2 activitie

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("enter your grade porcentage");
+        string LetterGrade = Console.ReadLine();
+
+        int LetterValue = int.Parse(LetterGrade);
+        string Letter = "";
+
+
+        if (LetterValue >= 90)
+        {
+            Letter = "A";
+        }
+        else if (LetterValue >= 80)
+        {
+            Letter = "B";
+        }
+        else if (LetterValue >= 70 && LetterValue < 80)
+        {
+            Letter = "C";
+        }
+        else if (LetterValue >= 60 && LetterValue < 70)
+        {
+            Letter = "D";
+        }
+        else
+        {
+            Letter = "F";
+        }
+        Console.WriteLine($"your grade is {Letter}.");
+
     }
 }
